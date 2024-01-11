ac3e994176afcdf9c54621d34d68eb463215124e
Miniii-git <125822323+Miniii-git@users.noreply.github.com>
2024-01-11 18:37:36 +0330

    add icon library

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import styles from "./Header.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBookOpen } from "@fortawesome/free-solid-svg-icons";
+import { PiBooksLight } from "react-icons/pi";
 
 function Header() {
   return (
     <div className={styles.Header}>
       <h1>
-        <FontAwesomeIcon icon={faBookOpen} /> Library App{" "}
+        <PiBooksLight id={styles.bookIcon} /> Library App{" "}
       </h1>
       <div id={styles.Mini}>
         <a href="#" target="-blank">
